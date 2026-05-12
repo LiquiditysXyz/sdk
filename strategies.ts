bdb3e8cce7c7ae9c56181a08293dcc6473f45eaa
Liquiditys <adeklincah72@gmail.com>
2026-05-13 00:01:31 +0700

    Create strategies.ts

---
+++ strategies.ts
@@ -0,0 +1,14 @@
+export const strategies = [
+  {
+    name: "Buyback",
+    allocation: 50
+  },
+  {
+    name: "Liquidity",
+    allocation: 30
+  },
+  {
+    name: "Burn",
+    allocation: 20
+  }
+];
