ae0fa0c9bad44b8163a331ba47c37ecc9b89bdeb
Liquiditys <adeklincah72@gmail.com>
2026-05-13 00:01:12 +0700

    Create Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-6 border-t border-zinc-800 bg-black text-zinc-500 text-center">
+      © 2026 Liquiditys. All rights reserved.
+    </footer>
+  );
+}
